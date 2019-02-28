8d58985bbcb696f8e7813b9ba767de56136adb6d
Orlando Rodriguez <creativerodriguez@gmail.com>
2019-02-28 10:20:25 -0500

    header.js component created
    header.js component created

--- react-microsite/src/App.js
+++ react-microsite/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+// components
+import Header from './components/headerComponent/header';
+
 import './assets/css/default.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Header />
       </div>
     );
   }
---
+++ react-microsite/src/components/headerComponent/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="Header">
+        Test My Header
+      </header>
+    );
+  }
+}
+
+export default Header;
